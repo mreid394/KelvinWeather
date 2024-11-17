65b3717fc46e8b919aa0efb6e3f39ab58ae28cc7
mreid301 <mreid301@caledonian.ac.uk>
2024-11-17 19:42:10 +0000

    Added program for temperature conversion across Celsius, Fahrenheit and Newton.

--- KelvinWeather/Program.cs
+++ KelvinWeather/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using static System.Formats.Asn1.AsnWriter;
+
+class Program
+{
+	static void Main()
+	{
+		const int kelvin = 293; // Creating a variable to store the value of Kelvin and setting it equal to 293. This is a constant variable as it will not change.
+		const int celsius = kelvin - 273; // Creating a variable to store the value of Celsius. As Celsius is 273 degrees less than Kelvin, we subtract 273 from the Kelvin value.
+		int fahrenheit = (int)Math.Floor(celsius * (9.0 / 5) + 32); // Creating a variable to store the value of Fahrenheit and is calculated using this formula. Math.Floor is used to round down the Fahrenheit value.
+		int newton = (int)Math.Floor(celsius * (33.0 / 100)); // Creating a variable to store the value of Newton and is calculated using this formula.
+
+		Console.WriteLine($"The temperature is {kelvin} degrees Kelvin."); // Output the original Kelvin value to the console.
+		Console.WriteLine($"The temperature is {celsius} degrees Celsius."); // Output the Celsius value to the console.
+		Console.WriteLine($"The temperature is {fahrenheit} degrees Fahrenheit."); // Output the calculated Fahrenheit value to the console.	
+		Console.WriteLine($"The temperature is {newton} degrees Newton."); // Output the calculated Newton value to the console.
+	}
+}
